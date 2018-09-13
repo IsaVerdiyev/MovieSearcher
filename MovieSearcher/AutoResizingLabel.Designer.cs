47849ca1f97186bf19f86e4a61573b89306b5a64
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-09-13 13:16:39 +0400

    Added Own Label

---
+++ MovieSearcher/AutoResizingLabel.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace MovieSearcher
+{
+    partial class AutoResizingLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 96);
+            this.checkedListBox1.TabIndex = 0;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
---
+++ MovieSearcher/AutoResizingLabel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieSearcher
+{
+    public partial class AutoResizingLabel : Label
+    {
+        #region  Public Constructors  
+
+        public AutoResizingLabel()
+        {
+            this.AutoSize = false;
+        }
+
+        #endregion  Public Constructors  
+
+        #region  Protected Overridden Methods  
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            this.FitToContents();
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+
+            this.FitToContents();
+        }
+
+        #endregion  Protected Overridden Methods  
+
+        #region  Protected Virtual Methods  
+
+        protected virtual void FitToContents()
+        {
+            Size size;
+
+            size = this.GetPreferredSize(new Size(this.Width, 0));
+
+            this.Height = size.Height;
+        }
+
+        #endregion  Protected Virtual Methods  
+
+        #region  Public Properties  
+
+        [DefaultValue(false), Browsable(false), EditorBrowsable(EditorBrowsableState.Never), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public override bool AutoSize
+        {
+            get { return base.AutoSize; }
+            set { base.AutoSize = value; }
+        }
+
+        #endregion  Public Properties  
+    }
+}
+
